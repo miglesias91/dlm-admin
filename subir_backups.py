28c5b5f0036321ce9e92b8df264a29c0cb4599c5
manu <m.iglesias91@gmail.com>
2021-03-27 19:07:31 -0300

    subir backups OK

--- subir_backups.py
+++ subir_backups.py
@@ -1,52 +1,30 @@
-from __future__ import print_function
 import os.path
-from googleapiclient.discovery import build
-from google_auth_oauthlib.flow import InstalledAppFlow
-from google.auth.transport.requests import Request
 from google.oauth2.credentials import Credentials
 from googleapiclient.http import MediaFileUpload
+from googleapiclient.discovery import build
 
 # If modifying these scopes, delete the file token.json.
 SCOPES = ['https://www.googleapis.com/auth/drive']
 
 def main():
-    """Shows basic usage of the Drive v3 API.
-    Prints the names and ids of the first 10 files the user has access to.
-    """
-    creds = None
-    # The file token.json stores the user's access and refresh tokens, and is
-    # created automatically when the authorization flow completes for the first
-    # time.
-    if os.path.exists('token.json'):
-        creds = Credentials.from_authorized_user_file('token.json', SCOPES)
-    # If there are no (valid) credentials available, let the user log in.
-    if not creds or not creds.valid:
-        if creds and creds.expired and creds.refresh_token:
-            creds.refresh(Request())
-        else:
-            flow = InstalledAppFlow.from_client_secrets_file('/home/manu/Descargas/credentials.json', SCOPES)
-            creds = flow.run_local_server(port=0)
-        # Save the credentials for the next run
-        with open('token.json', 'w') as token:
-            token.write(creds.to_json())
-
+    creds = Credentials.from_authorized_user_file('/home/ubuntu/keys/token.json', SCOPES)
     service = build('drive', 'v3', credentials=creds)
 
-    # Call the Drive v3 API
-    # results = service.files().list(pageSize=10, fields="nextPageToken, files(id, name)").execute()
-    # items = results.get('files', [])
+    string_ayer = (datetime.today() - timedelta(days=1)).strftime('%Y-%m-%d')
 
-    # if not items:
-    #     print('No files found.')
-    # else:
-    #     print('Files:')
-    #     for item in items:
-    #         print(u'{0} ({1})'.format(item['name'], item['id']))
-
-    file_metadata = {'name': 'notis-20200818-20200819.json'}
-    media = MediaFileUpload('/home/manu/Documentos/dlm-noticias-20200818-20200819.json', mimetype='text/json')
+    # subo noticias de ayer
+    name = 'backups_dlm/noticias/' + string_ayer + '.json'
+    path = '/home/ubuntu/backups_dlm/diarios/noticias/' + string_ayer + '.json'
+    metadata = {'name': name}
+    media = MediaFileUpload(path, mimetype='text/json')
     file = service.files().create(body=file_metadata, media_body=media, fields='id').execute()
-    print('File ID: {0}', file.get('id'))
+
+    # subo frecuencias de ayer
+    name = 'backups_dlm/frecuencias/' + string_ayer + '.json'
+    path = '/home/ubuntu/backups_dlm/diarios/noticias/' + string_ayer + '.json'
+    metadata = {'name': name}
+    media = MediaFileUpload(path, mimetype='text/json')
+    file = service.files().create(body=file_metadata, media_body=media, fields='id').execute()
 
 if __name__ == '__main__':
     main()
